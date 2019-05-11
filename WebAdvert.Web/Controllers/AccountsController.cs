e585f6b7634b5eddc82bd9dbd0893bbd1d122d2f
Pavel Mardouski <pmardouski@hotmail.com>
2019-05-11 14:37:43 +0300

    Initial commit/includes 9-14 topics

---
+++ WebAdvert.Web/Controllers/AccountsController.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Threading.Tasks;
+using Amazon.AspNetCore.Identity.Cognito;
+using Amazon.Extensions.CognitoAuthentication;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using WebAdvert.Web.Models.Accounts;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace WebAdvert.Web.Controllers
+{
+    public class AccountsController : Controller
+    {
+        private readonly SignInManager<CognitoUser> signInManager;
+        private readonly UserManager<CognitoUser> userManager;
+        private readonly CognitoUserPool userPool;
+
+        public AccountsController(SignInManager<CognitoUser> signInManager, UserManager<CognitoUser> userManager, CognitoUserPool userPool)
+        {
+            this.signInManager = signInManager ?? throw new ArgumentNullException(nameof(signInManager));
+            this.userManager = userManager ?? throw new ArgumentNullException(nameof(userManager));
+            this.userPool = userPool ?? throw new ArgumentNullException(nameof(userPool));
+        }
+
+        public async Task<IActionResult> Signup()
+        {
+            var model = new SignupModel();
+            return View(model);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Signup(SignupModel model)
+        {
+            if(ModelState.IsValid)
+            {
+                var user = userPool.GetUser(model.Email);
+                if (user.Status != null)
+                {
+                    ModelState.AddModelError("UserExists", "User with this email already exists!");
+                    return View(model);
+                }
+
+                user.Attributes.Add(CognitoAttribute.Name.AttributeName, model.Email); //todo: check!!!
+                var createdUser = await userManager.CreateAsync(user, model.Password);
+
+                if (createdUser.Succeeded)
+                {
+                    return RedirectToAction("Confirm");
+                }
+            }
+            
+            return View(model);
+        }
+
+        public async Task<IActionResult> Confirm()
+        {
+            var model = new ConfirmModel();
+
+            return View(model);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Confirm(ConfirmModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = await userManager.FindByEmailAsync(model.Email);
+                if (user == null)
+                {
+                    ModelState.AddModelError("NotFound", "A user with the given email was not found!");
+                    return View(model);
+                }
+
+                //(userManager as CognitoUserManager<CognitoUser>).ConfirmSignUpAsync(user, model.Code);
+                var result = await userManager.ConfirmEmailAsync(user, model.Email);
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Index", "Home");
+                }
+
+                foreach(var itm in result.Errors)
+                {
+                    ModelState.AddModelError(itm.Code, itm.Description);
+                }
+            }
+
+            return View(model);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Login(LoginModel model)
+        {
+            return View(model);
+        }
+
+        [HttpPost]
+        [ActionName("Login")]
+        public async Task<IActionResult> LoginPost(LoginModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var result = await signInManager.PasswordSignInAsync(model.Email, model.Password, model.RememberMe, false);
+
+                if (result.Succeeded)
+                    return RedirectToAction("Index", "Home");
+                else
+                    ModelState.AddModelError("LoginError", "Email or Password is incorrect");
+            }
+
+            return View("Login", model);
+        }
+    }
+}
---
+++ WebAdvert.Web/Startup.cs
@@ -0,0 +1,84 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace WebAdvert.Web
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.Configure<CookiePolicyOptions>(options =>
+            {
+                // This lambda determines whether user consent for non-essential cookies is needed for a given request.
+                options.CheckConsentNeeded = context => true;
+            });
+
+            services.AddCognitoIdentity(config =>
+            {
+                config.Password = new Microsoft.AspNetCore.Identity.PasswordOptions
+                {
+                    RequireDigit = true,
+                    RequiredLength = 6,
+                    RequireLowercase = true,
+                    RequireUppercase = true,
+                    RequiredUniqueChars = 0,
+                    RequireNonAlphanumeric = false
+                };
+            });
+            services.ConfigureApplicationCookie(option =>
+            {
+                option.LoginPath = "Accounts/Login";
+            });
+
+            services.AddControllersWithViews()
+                .AddNewtonsoftJson();
+            services.AddRazorPages();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
+
+            app.UseStaticFiles();
+
+            app.UseCookiePolicy();
+
+            app.UseAuthentication();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                endpoints.MapRazorPages();
+            });
+        }
+    }
+}
